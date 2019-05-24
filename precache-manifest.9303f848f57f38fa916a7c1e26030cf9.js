5644cb4ae3c871c25747e69202d61b43cb764c65
Rohit Man Shrestha <stha.rht028@gmail.com>
2019-05-24 13:30:25 +0545

    Updates

---
+++ precache-manifest.9303f848f57f38fa916a7c1e26030cf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e610d226831b3d67f23c9fe03c023de",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "ff8a3e3ea2626fe6e903",
+    "url": "/todo-app/static/css/main.6a25c4d5.chunk.css"
+  },
+  {
+    "revision": "a5863abf3272fa8c6b84",
+    "url": "/todo-app/static/js/2.ff830aa3.chunk.js"
+  },
+  {
+    "revision": "ff8a3e3ea2626fe6e903",
+    "url": "/todo-app/static/js/main.a6fad684.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
